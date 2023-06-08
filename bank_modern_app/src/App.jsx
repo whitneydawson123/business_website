572710a63c92e8eaa35142e61644ca06d1acdd12
Whitney <whitneydawson123@gmail.com>
2023-06-08 14:22:00 -0400

    finished navbar and hero section

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import styles from './style'
-import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components"
+import styles from './style';
+import {
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Navbar,
+  Stats,
+  Testimonials,
+  Hero,
+} from './components';
 
-const App = () => {
-  <div className='bg-primary w-full overflow-hidden'>
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`{styles.boxWidth}`}>
+      <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
 
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
     <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`styles.boxWidth`}>
+      <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
         <Billing />
@@ -23,6 +39,6 @@ const App = () => {
       </div>
     </div>
   </div>
-}
+);
 
-export default App
+export default App;
